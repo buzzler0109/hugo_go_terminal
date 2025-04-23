b8bb4aa3ca80009bd948a2fbb341e0ee621b479b
buzzler0109 <144600227+buzzler0109@users.noreply.github.com>
2025-04-23 04:09:41 +0300

    fix

--- tina/__generated__/client.ts
+++
@@ -1,5 +0,0 @@
-import { createClient } from "tinacms/dist/client";
-import { queries } from "./types";
-export const client = createClient({ url: 'http://localhost:4001/graphql', token: '177547ee6ba0f369c2a186d31e5443ed028f7e01', queries,  });
-export default client;
-  
