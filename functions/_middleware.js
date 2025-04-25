ee6e6ec7816b6bdb2dc60bcb3d52c4cca3af60d0
buzzler0109 <144600227+buzzler0109@users.noreply.github.com>
2025-04-25 19:18:22 +0300

    add function

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,7 +1,15 @@
 export async function onRequest(context) {
     const url = new URL(context.request.url);
+    
+    // Разрешаем все запросы к /admin
+    if (url.pathname.startsWith('/admin')) {
+      return context.next();
+    }
+    
+    // Разрешаем запросы к /auth
     if (url.pathname.startsWith('/auth')) {
       return context.next();
     }
+    
     return new Response('Not Found', { status: 404 });
   }
